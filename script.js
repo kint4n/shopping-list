e982931355306d2b0c45d5d4b4f84a7ac39ab044
Elkin Kristan <scyke1@nottingham.edu.cn>
2024-03-05 15:39:48 +0800

    Added update item functionality

--- script.js
+++ script.js
@@ -26,6 +26,17 @@ function onAddItemSubmit(e) {
         return;
     }
 
+    // Check for edit mode
+    if(isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode');
+
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('.edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
+
+    }
+
     // Create item DOM element
     addItemToDOM(newItem);
     
@@ -181,6 +192,8 @@ function filterItems(e) {
 
 // Remove item filter and clear all button when list is empty
 function checkUI() {
+    itemInput.value = '';
+
     const items = itemList.querySelectorAll('li');
 
     if(items.length === 0) {
@@ -191,6 +204,10 @@ function checkUI() {
         clearBtn.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+
+    isEditMode = false;
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor = '#333';
 }
 
 // Initialize App
